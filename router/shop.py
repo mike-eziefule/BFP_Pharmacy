e431f4e37527b9d5b4d27086b80590b286f1c5e9
mike-eziefule <mike.eziefule@gmail.com>
2024-06-14 17:23:15 +0100

    new branch dev

--- router/shop.py
+++ router/shop.py
@@ -1,5 +1,18 @@
-"""Routes related to User Account creation."""
+"""Routes related to online store and info."""
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 
 router = APIRouter(prefix="/shop", tags=["shop"])
+templates = Jinja2Templates(directory="templates")
+
+
+#shoppers welcome page route
+@router.get("/", response_class = HTMLResponse)
+async def store(
+    request:Request,
+):
+    return templates.TemplateResponse("shop.html", {"request": request})
